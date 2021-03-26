9e7704dc0cc756a8d98fe8cd8413ae0af44cfb3d
Bruno <holabrunogonzales@gmail.com>
2021-03-25 23:59:38 -0500

    About me

---
+++ layouts/default.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="flex justify-between p-4">
+    <Nuxt class="w-1/2" />
+    <ul class="w-1/3">
+      <li>Bruno Gonzales</li>
+      <li>
+        <nuxt-link to="/about">About</nuxt-link>
+      </li>
+    </ul>
+  </div>
+</template>
+<style>
+a[href] {
+  text-decoration: underline;
+  text-decoration-color: aqua;
+}
+</style>
---
+++ pages/about.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="flex flex-col space-y-4">
+    <img
+      src="https://pbs.twimg.com/profile_images/1230664341806026753/HbLTC7BE_400x400.jpg"
+      alt="photo of Bruno Gonzales"
+      class="object-contain h-48"
+    />
+    <p>
+      I live in Lima and work at
+      <a target="_blank" href="https://cluster.pe/">Cluster</a> . I grew up in
+      Peru and thaught myself programming. I work as a technical lead at Cluster
+      and co-host LimaJS community events.
+    </p>
+    <p>
+      I'm a proud father of three lovely cats and a beautiful woman who goes by
+      the name of Camila is the highlight of my everyday
+    </p>
+
+    <p>
+      Email:
+      <a href="mailto:holabrunogonzales@gmail.com"
+        >holabrunogonzales@gmail.com</a
+      >
+    </p>
+    <p>Elsewhere</p>
+    <p>
+      <a href="https://github.com/brunogonzales">brunogonzales</a> on GitHub;
+      <a href="https://twitter.com/vr1_____">vr1_____</a>
+      on Twitter
+    </p>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
